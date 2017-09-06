5b659bc4455da3b4216ee985b18990c26d42cce8
Dannychan129 <dannychan129@gmail.com>
2017-09-06 16:24:16 +0800

    first commit

---
+++ 造模版判断.js
@@ -0,0 +1,16 @@
+
+$.each(itemTepData, function (index, item) {
+    if (item.style.images) {
+
+        三小图的html结构
+    } else if (item.type == 'phvideo') {
+
+        大图的结构
+    } else if (item.thumbnail) {
+
+        左图右文的结构
+    } else {
+
+      纯文字的结构
+    }
+})
